3a544d53eea24483b37a3b132535b223addbbe4c
Papa-Pedro-13 <aleksandrostrovsqi@gmail.com>
2024-03-20 09:35:53 +0300

    pages

--- src/js/app.js
+++ src/js/app.js
@@ -57,30 +57,46 @@ new Tabs('tabs-example', {
 	},
 });
 
-// new Accordion('.accordion', {
-// 	shouldOpenAll: false, // true
-// 	defaultOpen: [], // [0,1]
-// 	collapsedClass: 'open',
-// });
+try {
+	new Accordion('.accordion', {
+		shouldOpenAll: false, // true
+		defaultOpen: [], // [0,1]
+		collapsedClass: 'open',
+	});
+} catch (err) {
+	console.log('No accordion')
+}
 
 
 $(document).ready((function () {
 	$(".slider__body").slick({
 		slidesToShow: 1,
 		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
 		arrows: !0,
 		fade: !0,
 		asNavFor: ".slider__nav"
-	}),
-		$(".slider__nav").slick({
-			slidesToShow: 3,
-			slidesToScroll: 1,
-			asNavFor: ".slider__body",
-			dots: !1,
-			arrows: !1,
-			focusOnSelect: !0,
-			variableWidth: !0
-		})
+	});
+	$(".slider__nav").slick({
+		slidesToShow: 5,
+		slidesToScroll: 1,
+
+		asNavFor: ".slider__body",
+		dots: !1,
+		arrows: !1,
+		focusOnSelect: !0,
+		variableWidth: !0,
+		responsive: [
+			{
+				breakpoint: 576,
+				settings: {
+					variableWidth: true,
+					centerMode: true,
+				}
+			},
+		]
+	});
 }))
 lightbox.option({
 	resizeDuration: 200,
